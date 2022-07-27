ee5b99ef982d6ef2dcaba29f0be4621ae68647a4
Russell Taylor <rctaylorllc@gmail.com>
2022-07-27 00:04:24 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,7 @@ install.packages("tidyverse")
 install.packages("jsonlite")
 library(tidyverse)
 
-###1
+###Deliverable 1: Linear Regression to Predict MPG
 
 library(dplyr)
 
@@ -17,7 +17,7 @@ lm_mpg
 
 summary(lm(lm_mpg)) #summarize linear model
 
-####2
+####Deliverable 2: Create Visualizations for the Trip Analysis
 
 Sus_coil_data <- read.csv('Suspension_Coil.csv') #import dataset
 
@@ -29,7 +29,25 @@ total_summary_sus_coil_data
 lot_summary_sus_coil_data <- Sus_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(PSI_Mean = mean(PSI), PSI_Median = median(PSI), PSI_Variance = var(PSI), PSI_Std_Dev = sd(PSI))
 lot_summary_sus_coil_data
 
-###3
+###Deliverable 3: T-Tests on Suspension Coils
+
+?t.test()
+?sample_n
+
+sample_1 <- Sus_coil_data %>% sample_n(40)
+qqnorm(sample_1)
+sample_2 <- Sus_coil_data %>% sample_n(40)
+
+#means of 2 samples
+t.test(sample_1$PSI, sample_2$PSI)
 
 
+
+#check if PSImean for each manufacturing lot is different from thepopulation mean of 1500
+
+t.test(Sus_coil_data$PSI, mu = 1500)
+
+t.test(subset(Sus_coil_data, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(Sus_coil_data, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(Sus_coil_data, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
 
